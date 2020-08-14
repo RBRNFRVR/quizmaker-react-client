7dbc7430b1aea9a16e8bb52321643f21e90dc074
alexpac27 <alexpac27@gmail.com>
2020-08-14 12:34:24 -0400

    question cards rendering

--- src/QuestionCard.js
+++ src/QuestionCard.js
@@ -1,14 +1,23 @@
 import React from 'react'
 
 class QuestionCard extends React.Component{
+
+    addToQuiz = () => {
+        console.log("added to quiz!")
+        fetch()
+    }
+
     render(){
-        console.log("in qC", this.props.question)
+        
         return(
             <div>
                 <h3>Question</h3>
+                <p>Category: {this.props.category}</p>
+                <p>Difficulty: {this.props.difficulty}</p>
                 <p>Question: {this.props.question}</p>
-                <p>Correct Answer:{this.props.correctAnswer}</p>
-                <p>Incorrect Answers: {this.props.incorrectAnswers}</p>
+                <p>Correct Answer: {this.props.correctAnswer}</p>
+                <p>Incorrect Answers: {this.props.incorrectAnswers.join(", ")}</p>
+                <button onClick={this.addToQuiz}>Add to Quiz</button>
             </div>
         )
     }
--- src/Quizmaker.js
+++ src/Quizmaker.js
@@ -6,17 +6,13 @@ import QuestionCard from './QuestionCard'
 
 class Quizmaker extends Component {
 
-    // renderCards = () => {
-    //     this.props.questions.map(obj => <QuestionCard question={obj.question} correctAnswer={obj.correct_answer} incorrectAnswers={obj.incorrect_answers}/>)
-    // }
-
     render(){
         console.log("in maker", this.props.questions)
         return(
             <div>
                 <h1>Quizmaker Container</h1>
                 
-                {this.props.questions.map(obj => <QuestionCard question={obj.question} correctAnswer={obj.correct_answer} incorrectAnswers={obj.incorrect_answers}/>)}
+                {this.props.questions.map(obj => <QuestionCard category={obj.category} difficulty={obj.difficulty} question={obj.question} correctAnswer={obj.correct_answer} incorrectAnswers={obj.incorrect_answers}/>)}
                 
             </div>
         )
