4612b3225e7d64402e723a9f7742ece353dedcf6
RBRNFRVR <rommelchavez99@gmail.com>
2020-08-17 14:33:39 -0400

    navbardone

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,23 +11,25 @@ class Navbar extends Component {
     }
     handleonMouseEnter =(event) => {
        this.state.hover === true? this.setState({hover:false}):this.setState({hover:true})
-       
+
     }
 
     render(){
         return(
             <div className={styles.NavbarFlex}>
+                <div className={styles.leftthree}>
                 <div className={styles.buttonlink}>
-                <Link to='/profile' onMouseEnter={this.handleonMouseEnter}>Profile</Link>
+                <Link to='/profile' onMouseEnter={this.handleonMouseEnter} className={styles.linkstyle}>Profile</Link>
                 </div>
                 <div className={styles.buttonlink}>
-                <Link to='/quizmaker' onClick={this.props.qmClicked} onMouseEnter={this.handleonMouseEnter}>Quizmaker</Link>
+                <Link to='/quizmaker' onClick={this.props.qmClicked} onMouseEnter={this.handleonMouseEnter} className={styles.linkstyle}>Quizmaker</Link>
                 </div>
                 <div className={styles.buttonlink}>
-                <Link to='/quiztaker'onMouseEnter={this.handleonMouseEnter}>Quiztaker</Link>
+                <Link to='/quiztaker'onMouseEnter={this.handleonMouseEnter} className={styles.linkstyle}>Quiztaker</Link>
+                </div>
                 </div>
                 <div className={styles.buttonlink}>
-                <Link to='/loginform'onMouseEnter={this.handleonMouseEnter}>Login</Link>
+                <Link to='/loginform'onMouseEnter={this.handleonMouseEnter} className={styles.loginlinkstyle}>Login</Link>
                 </div>
             </div>
         )
