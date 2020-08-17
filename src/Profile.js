9f36abf87ba03321e356f76b3b543e5ccafd07a1
RBRNFRVR <rommelchavez99@gmail.com>
2020-08-17 10:49:47 -0400

    fixprofile

--- src/Profile.js
+++ src/Profile.js
@@ -80,7 +80,8 @@ class Profile extends Component{
                 <h2>Username: {this.state.info.username}</h2>
                 <p>My Quizzes:</p>
                     <ul>{(this.state.quizNames === "" ? null : this.state.quizNames.map(obj => <li onClick={() => this.clickedQuiz(obj)}>{obj}</li>))}</ul> 
-                <div className={styles.QuizmakerQuestionsDisplay}>
+                {/* <div className={styles.QuizmakerQuestionsDisplay}> */}
+                <div>
                     { (!this.state.quizClicked ? <h4>Select a quiz from "My Quizzes" to view more details</h4> : <div> <h3>{this.state.quizName}</h3><button>Delete Quiz</button></div>)}  
                     { (this.state.filteredQuestions === "" ? null : this.state.filteredQuestions.map(obj =>  {
                     counter += 1
