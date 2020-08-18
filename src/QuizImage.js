e19e282f4be0891538bc63b76061a7ad2b6568a8
alexpac27 <alexpac27@gmail.com>
2020-08-18 14:39:47 -0400

    changed file location

--- src/QuizImage.js
+++ src/QuizImage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styles from './mystyle.module.css'
-import image from '/Users/rbrnfrvr/Mod-4-Project-Redos/Front-End/quizmaker-react-client/src/03dbf00e-9264-414f-9565-1b6d4de12aba_200x200 (1).png'
+import image from './03dbf00e-9264-414f-9565-1b6d4de12aba_200x200 (1).png'
 
 
 class QuizImage extends Component{
